602222133aa830539b9c045cc409d576f259df1f
jarvisoa <jarvisoa@rose-hulman.edu>
2015-10-18 22:39:18 -0400

    sup i got PUT

--- src/protocol/PutRequestHandler.java
+++ src/protocol/PutRequestHandler.java
@@ -25,21 +25,60 @@
  * NY 13699-5722
  * http://clarkson.edu/~rupakhcr
  */
- 
+
 package protocol;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * 
  * @author Chandan R. Rupakheti (rupakhcr@clarkson.edu)
  */
-public class PutRequestHandler implements IRequestHandler{
-	/* (non-Javadoc)
-	 * @see protocol.IRequestHandler#handleRequest(protocol.HttpRequest, java.lang.String)
+public class PutRequestHandler implements IRequestHandler {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see protocol.IRequestHandler#handleRequest(protocol.HttpRequest,
+	 * java.lang.String)
 	 */
 	@Override
 	public HttpResponse handleRequest(HttpRequest request, String rootDir) {
-		// TODO Auto-generated method stub
-		return null;
+
+		HttpResponse response = null;
+		
+		String uri = request.getUri();
+		File file = new File(rootDir + uri);
+		
+		if(file.exists()) 
+		{
+			if(file.isDirectory())
+			{
+				response = HttpResponseFactory.create404NotFound(Protocol.CLOSE);
+			}
+			else {
+				if(file.exists()) {
+					file.delete();
+					response = HttpResponseFactory.create200OK(file, Protocol.CLOSE);
+				}
+			}
+		}
+		else {
+			response = HttpResponseFactory.create404NotFound(Protocol.CLOSE);
+		}
+		
+		try {
+			file.createNewFile();
+			FileOutputStream writer = new FileOutputStream(file, true);
+			String contents = new String(request.getBody());
+			writer.write(contents.getBytes());
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return response;
 	}
 
 }
